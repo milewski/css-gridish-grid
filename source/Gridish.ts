6e6fd0eb4044d7dc529c36951824b72dc8b6768c
milewski <rafael.milewski@gmail.com>
2018-03-13 11:27:08 +0800

    offset grid top by 1px

--- source/Gridish.ts
+++ source/Gridish.ts
@@ -176,7 +176,7 @@ export default class Gridish {
             lineHeight: 'normal',
             left: '0',
             right: '0',
-            top: '0',
+            top: '1px',
             zIndex: '9999'
         })
 
